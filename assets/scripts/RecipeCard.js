d0078be160894f01aac3aa67a222a0557f56e142
wltmd153 <103233907+wltmd153@users.noreply.github.com>
2022-11-08 14:35:34 -0800

    Update RecipeCard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -10,7 +10,7 @@ class RecipeCard extends HTMLElement {
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
     let shadowEl = this.attachShadow({mode: 'open'});
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
-    let article = document.createElment('article');
+    let article = document.createElement('article');
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
     let style = document.createElement('style');
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
