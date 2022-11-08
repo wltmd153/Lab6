64857d62ba5de970984e160d399aefd9a62faac9
wltmd153 <103233907+wltmd153@users.noreply.github.com>
2022-11-08 14:12:23 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -60,7 +60,7 @@ function saveRecipesToStorage(recipes) {
   // B1. TODO - Complete the functionality as described in this function
   //            header. It is possible in only a single line, but should
   //            be no more than a few lines.
-  let recipeString = recipes.stringify(recipes);
+  let recipeString = JSON.stringify(recipes);
   localStorage.setItem('recipes', recipeString);
 }
 
